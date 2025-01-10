d4570a2466b734041ccace9990a3eccd6865a74d
Joseph Ofori Tenkorang <83918984+mrtenkorang@users.noreply.github.com>
2025-01-10 17:50:15 +0000

    refactored

---
+++ cocoa_rehhab_monitor/Cocoa-Monitor-2.1/lib/controller/db/rehab_assistant_db.dart
@@ -0,0 +1,146 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../model/rehab_assistant_model.dart';
+
+class RehabAssistantDatabaseHelper {
+  static final RehabAssistantDatabaseHelper instance =
+  RehabAssistantDatabaseHelper._init();
+
+  static Database? _database;
+
+  RehabAssistantDatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('rehab_assistant.db');
+    return _database!;
+  }
+
+  // Table and Column Definitions
+  static const String tableName = "rehab_assistant";
+
+  final String id = 'id';
+  final String rehabName = 'rehab_name';
+  final String rehabCode = 'rehab_code';
+  final String phoneNumber = 'phone_number';
+  final String salaryBankName = 'salary_bank_name';
+  final String bankAccountNumber = 'bank_account_number';
+  final String gender = 'gender';
+  final String ssnitNumber = 'ssnit_number';
+  final String momoNumber = 'momo_number';
+  final String momoAccountName = 'momo_account_name';
+  final String dob = 'dob';
+  final String poFirstName = 'po_first_name';
+  final String poLastName = 'po_last_name';
+  final String districtName = 'district_name';
+  final String districtId = 'district_id';
+  final String image = 'image';
+  final String paymentOption = 'payment_option';
+  final String designation = 'designation';
+  final String regionId = 'region_id';
+  final String regionName = 'region_name';
+  final String staffId = 'staff_id';
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $tableName (
+        $id INTEGER PRIMARY KEY AUTOINCREMENT,
+        $rehabName TEXT,
+        $rehabCode INTEGER,
+        $phoneNumber TEXT,
+        $salaryBankName TEXT,
+        $bankAccountNumber TEXT,
+        $gender TEXT,
+        $ssnitNumber TEXT,
+        $momoNumber TEXT,
+        $momoAccountName TEXT,
+        $dob TEXT,
+        $poFirstName TEXT,
+        $poLastName TEXT,
+        $districtName TEXT,
+        $districtId INTEGER,
+        $image TEXT,
+        $paymentOption TEXT,
+        $designation TEXT,
+        $regionId TEXT,
+        $regionName TEXT,
+        $staffId TEXT
+      )
+    ''');
+  }
+
+  Future<int> saveData(RehabAssistantModel data) async {
+    final db = await instance.database;
+    return await db.insert(tableName, data.toJson());
+  }
+
+  Future<List<RehabAssistantModel>> getAllData() async {
+    final db = await instance.database;
+    final result = await db.query(tableName);
+    return result.map((json) => RehabAssistantModel.fromJson(json)).toList();
+  }
+
+  Future<RehabAssistantModel?> getDataById(int id) async {
+    final db = await instance.database;
+    final result = await db.query(
+      tableName,
+      where: '$id = ?',
+      whereArgs: [id],
+    );
+    return result.isNotEmpty
+        ? RehabAssistantModel.fromJson(result.first)
+        : null;
+  }
+
+  Future<int> updateData(RehabAssistantModel data) async {
+    final db = await instance.database;
+    return await db.update(
+      tableName,
+      data.toJson(),
+      where: '$id = ?',
+      whereArgs: [data.id],
+    );
+  }
+
+  Future<int> deleteData(int id) async {
+    final db = await instance.database;
+    return await db.delete(tableName, where: '$id = ?', whereArgs: [id]);
+  }
+
+  Future<int> deleteAll() async {
+    final db = await instance.database;
+    return await db.delete(tableName);
+  }
+
+  Future<int> bulkInsertData(List<RehabAssistantModel> allData) async {
+    final db = await instance.database;
+    int count = 0;
+    await db.transaction((txn) async {
+      for (final data in allData) {
+        // Convert the model to a Map
+        final dataMap = data.toJson();
+
+        await txn.insert(tableName, dataMap);
+        count++;
+      }
+    });
+    return count;
+  }
+
+  Future<void> close() async {
+    final db = await instance.database;
+    await db.close();
+  }
+}
---
+++ cocoa_rehhab_monitor/Cocoa-Monitor-2.1/lib/controller/model/rehab_assistant_model.dart
@@ -0,0 +1,107 @@
+// To parse this JSON data, do
+//
+//     final rehabAssistant = rehabAssistantFromJson(jsonString);
+
+import 'dart:convert';
+
+import 'package:floor/floor.dart';
+
+RehabAssistantModel rehabAssistantFromJsonModel(String str) =>
+    RehabAssistantModel.fromJson(json.decode(str));
+
+String rehabAssistantToJsonModel(RehabAssistantModel data) => json.encode(data.toJson());
+
+class RehabAssistantModel {
+  RehabAssistantModel({
+    this.id,
+    this.rehabName,
+    this.rehabCode,
+    this.phoneNumber,
+    this.salaryBankName,
+    this.bankAccountNumber,
+    this.gender,
+    this.ssnitNumber,
+    this.momoNumber,
+    this.momoAccountName,
+    this.dob,
+    this.poFirstName,
+    this.poLastName,
+    this.districtName,
+    this.districtId,
+    this.image,
+    this.paymentOption,
+    this.designation,
+    this.regionId,
+    this.regionName,
+    this.staffId,
+  });
+
+  final int? id;
+
+  String? rehabName;
+  int? rehabCode;
+  String? phoneNumber;
+  String? salaryBankName;
+  String? bankAccountNumber;
+  String? gender;
+  String? ssnitNumber;
+  String? momoNumber;
+  String? momoAccountName;
+  String? dob;
+  String? poFirstName;
+  String? poLastName;
+  String? districtName;
+  int? districtId;
+  String? image;
+  String? paymentOption;
+  String? designation;
+  String? regionId;
+  String? regionName;
+  String? staffId;
+
+  factory RehabAssistantModel.fromJson(Map<String, dynamic> json) => RehabAssistantModel(
+    rehabName: json["name"],
+    staffId: json["staff_code"],
+    phoneNumber: json["phone_number"],
+    salaryBankName: json["bank_branch"],
+    bankAccountNumber: json["bank_account_number"],
+    gender: json['gender'],
+    ssnitNumber: json["ssnit_number"],
+    momoNumber: json["momo_number"],
+    momoAccountName: json["momo_account_name"],
+    dob: json["dob"],
+    poFirstName: json["po_first_name"],
+    poLastName: json["po_last_name"],
+    districtName: json["district"],
+    districtId: json["district_id"],
+    image: json["photo_staff"] ?? "",
+    paymentOption: json["payment_option"],
+    designation: json["designation"],
+    regionId: json["region_id"],
+    regionName: json["region_name"],
+    rehabCode: json["rehab_code"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": rehabName,
+    "staff_code": staffId,
+    "phone_number": phoneNumber,
+    "bank_branch": salaryBankName,
+    "bank_account_number": bankAccountNumber,
+    "gender": gender,
+    "ssnit_number": ssnitNumber,
+    "momo_number": momoNumber,
+    "momo_account_name": momoAccountName,
+    "dob": dob,
+    "po_first_name": poFirstName,
+    "po_last_name": poLastName,
+    "district": districtName,
+    "district_id": districtId,
+    "photo_staff": image,
+    "payment_option": paymentOption,
+    "designation": designation,
+    "region_id": regionId,
+    "region_name": regionName,
+    "rehab_code": rehabCode,
+  };
+}
