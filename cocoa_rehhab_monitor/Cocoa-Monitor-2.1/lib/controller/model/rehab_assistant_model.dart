38d14799a42dc6754b8dbc3ea6b30c8f56ce46cc
Joseph Ofori Tenkorang <83918984+mrtenkorang@users.noreply.github.com>
2025-01-12 12:40:52 +0000

    refactored

--- cocoa_rehhab_monitor/Cocoa-Monitor-2.1/lib/controller/model/rehab_assistant_model.dart
+++ cocoa_rehhab_monitor/Cocoa-Monitor-2.1/lib/controller/model/rehab_assistant_model.dart
@@ -1,107 +1,87 @@
-// To parse this JSON data, do
-//
-//     final rehabAssistant = rehabAssistantFromJson(jsonString);
-
 import 'dart:convert';
-
-import 'package:floor/floor.dart';
-
-RehabAssistantModel rehabAssistantFromJsonModel(String str) =>
-    RehabAssistantModel.fromJson(json.decode(str));
-
-String rehabAssistantToJsonModel(RehabAssistantModel data) => json.encode(data.toJson());
 
 class RehabAssistantModel {
   RehabAssistantModel({
-    this.id,
-    this.rehabName,
     this.rehabCode,
+    this.rehabName,
+    this.districtId,
+    this.districtName,
+    this.regionId,
+    this.regionName,
+    this.designation,
+    this.image,
+    this.staffId,
     this.phoneNumber,
     this.salaryBankName,
     this.bankAccountNumber,
-    this.gender,
     this.ssnitNumber,
-    this.momoNumber,
     this.momoAccountName,
-    this.dob,
-    this.poFirstName,
-    this.poLastName,
-    this.districtName,
-    this.districtId,
-    this.image,
+    this.momoNumber,
     this.paymentOption,
-    this.designation,
-    this.regionId,
-    this.regionName,
-    this.staffId,
+    this.po,
   });
 
-  final int? id;
+  final int? rehabCode;
+  final String? rehabName;
+  final int? districtId;
+  final String? districtName;
+  final String? regionId;
+  final String? regionName;
+  final String? designation;
+  final String? image;
+  final String? staffId;
+  final String? phoneNumber;
+  final String? salaryBankName;
+  final String? bankAccountNumber;
+  final String? ssnitNumber;
+  final String? momoAccountName;
+  final String? momoNumber;
+  final String? paymentOption;
+  final String? po;
 
-  String? rehabName;
-  int? rehabCode;
-  String? phoneNumber;
-  String? salaryBankName;
-  String? bankAccountNumber;
-  String? gender;
-  String? ssnitNumber;
-  String? momoNumber;
-  String? momoAccountName;
-  String? dob;
-  String? poFirstName;
-  String? poLastName;
-  String? districtName;
-  int? districtId;
-  String? image;
-  String? paymentOption;
-  String? designation;
-  String? regionId;
-  String? regionName;
-  String? staffId;
+  // From JSON
+  factory RehabAssistantModel.fromJson(Map<String, dynamic> json) =>
+      RehabAssistantModel(
+        rehabCode: json["rehab_code"],
+        rehabName: json["rehab_name"],
+        districtId: json["district_id"],
+        districtName: json["district_name"],
+        regionId: json["region_id"],
+        regionName: json["region_name"],
+        designation: json["designation"],
+        image: json["image"],
+        staffId: json["staff_id"],
+        phoneNumber: json["phone_number"],
+        salaryBankName: json["salary_bank_name"],
+        bankAccountNumber: json["bank_account_number"],
+        ssnitNumber: json["ssnit_number"],
+        momoAccountName: json["momo_account_name"],
+        momoNumber: json["momo_number"],
+        paymentOption: json["payment_option"],
+        po: json["po"],
+      );
 
-  factory RehabAssistantModel.fromJson(Map<String, dynamic> json) => RehabAssistantModel(
-    rehabName: json["name"],
-    staffId: json["staff_code"],
-    phoneNumber: json["phone_number"],
-    salaryBankName: json["bank_branch"],
-    bankAccountNumber: json["bank_account_number"],
-    gender: json['gender'],
-    ssnitNumber: json["ssnit_number"],
-    momoNumber: json["momo_number"],
-    momoAccountName: json["momo_account_name"],
-    dob: json["dob"],
-    poFirstName: json["po_first_name"],
-    poLastName: json["po_last_name"],
-    districtName: json["district"],
-    districtId: json["district_id"],
-    image: json["photo_staff"] ?? "",
-    paymentOption: json["payment_option"],
-    designation: json["designation"],
-    regionId: json["region_id"],
-    regionName: json["region_name"],
-    rehabCode: json["rehab_code"],
-  );
-
+  // To JSON
   Map<String, dynamic> toJson() => {
-    "name": rehabName,
-    "staff_code": staffId,
-    "phone_number": phoneNumber,
-    "bank_branch": salaryBankName,
-    "bank_account_number": bankAccountNumber,
-    "gender": gender,
-    "ssnit_number": ssnitNumber,
-    "momo_number": momoNumber,
-    "momo_account_name": momoAccountName,
-    "dob": dob,
-    "po_first_name": poFirstName,
-    "po_last_name": poLastName,
-    "district": districtName,
+    "rehab_code": rehabCode,
+    "rehab_name": rehabName,
     "district_id": districtId,
-    "photo_staff": image,
-    "payment_option": paymentOption,
-    "designation": designation,
+    "district_name": districtName,
     "region_id": regionId,
     "region_name": regionName,
-    "rehab_code": rehabCode,
+    "designation": designation,
+    "image": image,
+    "staff_id": staffId,
+    "phone_number": phoneNumber,
+    "salary_bank_name": salaryBankName,
+    "bank_account_number": bankAccountNumber,
+    "ssnit_number": ssnitNumber,
+    "momo_account_name": momoAccountName,
+    "momo_number": momoNumber,
+    "payment_option": paymentOption,
+    "po": po,
   };
+
+  static List<RehabAssistantModel> fromJsonList(List<dynamic> jsonList) =>
+      jsonList.map((json) => RehabAssistantModel.fromJson(json)).toList();
 }
